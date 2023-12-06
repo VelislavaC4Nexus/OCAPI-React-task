2e33c434045dca706ee191c408f56e41f42053f0
VelislavaC4Nexus <velislava.marinova.c4Nexus@gmail.com>
2023-12-06 06:13:52 +0200

    OCAPI-React-task

---
+++ my-app/src/App.js
@@ -0,0 +1,15 @@
+import Header from './header/Header';
+import Footer from './footer/Footer';
+import { Route, Routes } from "react-router-dom";
+import Home from './home/Home';
+import ProductDetails from './products/productDetails';
+
+function App() {
+  return (
+    <div>
+   
+    </div>
+  )
+}
+
+export default App;
